0efaef72d77779b6d575abf45f2cd3338a0b3e8f
Jaap Jansma <info@edeveloper.nl>
2014-03-18 13:14:30 +0100

    error file

--- src/CiviCoop/VragenboomBundle/Form/ActieDefinitieType.php
+++ src/CiviCoop/VragenboomBundle/Form/ActieDefinitieType.php
@@ -6,31 +6,28 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class ActieDefinitieType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('object', 'entity', array(
-				'class' => 'CiviCoopVragenboomBundle:Object',
-				'property' => 'naam',
-			))
-			->add('actie')
-->add('verantwoordelijke', 'choice', array('choices' => array('De Goede Woning' => 'De Goede Woning', 'Huurder' => 'Huurder'), 'required' => true))
-            ->add('description')
-            
-        ;
-    }
+class ActieDefinitieType extends AbstractType {
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'CiviCoop\VragenboomBundle\Entity\ActieDefinitie'
-        ));
-    }
+  public function buildForm(FormBuilderInterface $builder, array $options) {
+    $builder
+        ->add('object', 'entity', array(
+          'class' => 'CiviCoopVragenboomBundle:Object',
+          'property' => 'naam',
+        ))
+        ->add('actie')
+        ->add('verantwoordelijke', 'choice', array('choices' => array('De Goede Woning' => 'De Goede Woning', 'Huurder' => 'Huurder'), 'required' => true))
+        ->add('description')
+    ;
+  }
 
-    public function getName()
-    {
-        return 'civicoop_vragenboombundle_actiedefinitietype';
-    }
+  public function setDefaultOptions(OptionsResolverInterface $resolver) {
+    $resolver->setDefaults(array(
+      'data_class' => 'CiviCoop\VragenboomBundle\Entity\ActieDefinitie'
+    ));
+  }
+
+  public function getName() {
+    return 'civicoop_vragenboombundle_actiedefinitietype';
+  }
+
 }
