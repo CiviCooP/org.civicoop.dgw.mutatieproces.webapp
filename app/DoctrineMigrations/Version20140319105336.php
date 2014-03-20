358c98f2faef719f001336d5daf2c7d8af9a7695
Jaap Jansma <info@edeveloper.nl>
2014-03-20 09:06:34 +0100

    fix in migrations script

--- app/DoctrineMigrations/Version20140319105336.php
+++ app/DoctrineMigrations/Version20140319105336.php
@@ -15,7 +15,6 @@ class Version20140319105336 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
-        $this->addSql("ALTER TABLE ActieDefinitie DROP `default`");
         $this->addSql("ALTER TABLE Client ADD phone VARCHAR(255) DEFAULT NULL");
     }
 
@@ -24,7 +23,6 @@ class Version20140319105336 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
-        $this->addSql("ALTER TABLE ActieDefinitie ADD `default` TINYINT(1) NOT NULL");
         $this->addSql("ALTER TABLE Client DROP phone");
     }
 }
